ed955b6ddff64cf78476e8ae5f147e2ed25a3a43
CJH <328692149@qq.com>
2022-09-22 10:26:36 +0800

    first commit

---
+++ src/test/java/com/cjh/boot/Boot05AdminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjh.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05AdminserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
